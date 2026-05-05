6ae0cdb3526d30f331acf8e50b5ac9c4f3dd3ea9
DiegoSB20 <diego.seco@alumnos.ucn.cl>
2026-05-04 21:14:04 -0400

    se crea el Commit Reproductor.hpp

--- Core/Reproductor.hpp
+++ Core/Reproductor.hpp
@@ -1,1 +1,43 @@
+#ifndef REPRODUCTOR_HPP
+#define REPRODUCTOR_HPP
 
+#include "../Classes/Song.h"
+#include "../Data_structure/Linkedlist.h"
+#include "../Classes/Filemanager.h"
+#include <string>
+
+class Reproductor {
+private:
+    LinkedList<Song> fonoteca;
+    LinkedList<Song>::Node* nodoActual;
+
+    int estadoActual;     // 0 = Detenida, 1 = Reproduciendo, 2 = Pausada
+    int modoRepeticion;   // 0 = Desactivado, 1 = Repetir una, 2 = Repetir todas
+    bool modoAleatorio;
+
+    std::string rutaArchivo;
+
+    void limpiarPantalla();
+    std::string obtenerTextoEstado() const; 
+
+    void mostrarMenuPrincipal();
+    void procesarEntradaPrincipal(const std::string& entrada);
+
+    void mostrarSubmenuL();
+    void mostrarSubmenuA();
+
+
+    void alternarPlayPausa();
+    void siguienteCancion();
+    void anteriorCancion();
+    void cambiarRepeticion();
+    void agregarNuevaCancion();
+
+public:
+    Reproductor(std::string ruta);
+    ~Reproductor();
+
+    void iniciar();
+};
+
+#endif
